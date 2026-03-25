2cd46f18c61f9d2e9272fa3523d7ac66d2e3b008
Leplusbio225 <kacouemmanuel225@yahoo.com>
2026-03-25 22:28:21 +0000

    refactor: suppression du champ 'role' et ajout de la clé étrangère 'role_id' dans la migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,9 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('role_id')
+                ->constrained()
+                ->cascadeOnDelete();
             $table->string('name');
             $table->string('email')->unique();
-            $table->enum('role', ['admin', 'manager'])->default('user');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
